2c512a3f7fb64ec38dbcacc4f88237140a3c81f1
yasuHs <maple9711119@gmail.com>
2022-09-14 17:16:09 +0800

    bug fix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function cal() {
     kg = document.getElementById("kg").value;
     result = kg * 5.658 + 0.0204;
-    result = Math.round(result * 100000) / 100000;
+    result = Math.round(result * 100000) / 100000 / 1000;
     document.getElementById("result").textContent = result;
 }
