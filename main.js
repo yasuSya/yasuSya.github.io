132b5593c79f604139440a624e73f54ef6c713a0
yasuHs <maple9711119@gmail.com>
2022-09-14 17:07:16 +0800

    upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function cal() {
+    kg = document.getElementById("kg").value;
+    result = kg * 5.658 + 0.0204;
+    result = Math.round(result * 100000) / 100000;
+    document.getElementById("result").textContent = result;
+}
